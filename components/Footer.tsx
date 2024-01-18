af1f86eb827c1984d70e6b7d37a399d05d7f011c
Eddy <id.hemedy98@gmail.com>
2024-01-18 12:53:38 +0200

    Working on the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-2 max-md:flex-col'>
+      <p>Copyright &copy; 2024 Idrissa | All Rights Reserved</p>
+      <div className='flex gap-x-9'>
+        <Link href='#' className='text-body'>Terms & Conditions</Link>
+        <Link href='#' className='text-body'>Privacy Policy</Link>
+      </div>
+    </footer>
   )
 }
 
